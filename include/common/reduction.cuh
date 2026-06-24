e7f2c415669d9338fa69b924316b3a1dde42cd02
Lu Tracy <793910479@qq.com>
2026-06-24 12:43:29 +0800

    refactor(kernel): 提取通用 reduce 原语到 common/reduction.cuh
    将 rmsnorm.cu 中的 AddOp 与 shuffle_warp_reduce 迁移至
    include/common/reduction.cuh，便于后续 kernel 复用。

---
+++ include/common/reduction.cuh
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cuda_runtime.h>
+
+template <typename T>
+struct AddOp
+{
+    static __host__ __device__ T invoke(T a, T b) { return a + b; }
+};
+
+template <int warp_size, typename T, template <typename> class Op>
+__device__ __forceinline__ T shuffle_warp_reduce(T x)
+{
+    constexpr unsigned int mask = (1ULL << warp_size) - 1;
+    if (32 <= warp_size)
+        x = Op<T>::invoke(x, __shfl_down_sync(mask, x, 16));
+    if (16 <= warp_size)
+        x = Op<T>::invoke(x, __shfl_down_sync(mask, x, 8));
+    if (8 <= warp_size)
+        x = Op<T>::invoke(x, __shfl_down_sync(mask, x, 4));
+    if (4 <= warp_size)
+        x = Op<T>::invoke(x, __shfl_down_sync(mask, x, 2));
+    if (2 <= warp_size)
+        x = Op<T>::invoke(x, __shfl_down_sync(mask, x, 1));
+    return x;
+}
--- src/kernel/rmsnorm.cu
+++ src/kernel/rmsnorm.cu
@@ -2,30 +2,8 @@
 #include <core/Tensor.cuh>
 #include <common/types.h>
 #include <common/config.h>
+#include <common/reduction.cuh>
 #include <cuda_fp16.h>
-
-template <typename T>
-struct AddOp
-{
-    static __host__ __device__ T invoke(T a, T b) { return a + b; }
-};
-
-template <int warp_size, typename T, template <typename> class Op>
-__device__ __forceinline__ T shuffle_warp_reduce(T x)
-{
-    constexpr unsigned int mask = (1ULL << warp_size) - 1;
-    if (32 <= warp_size)
-        x = Op<T>::invoke(x, __shfl_down_sync(mask, x, 16));
-    if (16 <= warp_size)
-        x = Op<T>::invoke(x, __shfl_down_sync(mask, x, 8));
-    if (8 <= warp_size)
-        x = Op<T>::invoke(x, __shfl_down_sync(mask, x, 4));
-    if (4 <= warp_size)
-        x = Op<T>::invoke(x, __shfl_down_sync(mask, x, 2));
-    if (2 <= warp_size)
-        x = Op<T>::invoke(x, __shfl_down_sync(mask, x, 1));
-    return x;
-}
 
 template <typename T>
 __global__ void rmsnorm_kernel(const unsigned int num_input_tokens,

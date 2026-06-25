e1719b34f7d4911b561f4497a6f80e68b7b65a84
Lu Tracy <793910479@qq.com>
2026-06-25 12:34:39 +0800

    调整数值稳定性常量 EPS 为 1e-5

--- include/common/config.h
+++ include/common/config.h
@@ -4,4 +4,4 @@ constexpr unsigned int THREADS_PER_BLOCK = 512;
 constexpr unsigned int WARP_SIZE = 32;
 constexpr unsigned int WARPS_PER_BLOCK = THREADS_PER_BLOCK / WARP_SIZE;
 constexpr unsigned int NUM_BLOCKS = 256;
-constexpr float EPS = 1e-6f;
+constexpr float EPS = 1e-5f;

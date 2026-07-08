b20ae33e1f9e4c4a1f15075f8d95b9b47f134f76
pentatracy5 <pentatracy5@bupt.cn>
2026-07-08 13:35:24 +0800

    给 build_causal_mask 增加必要的注释

--- include/kernel/build_causal_mask.h
+++ include/kernel/build_causal_mask.h
@@ -6,6 +6,6 @@ class Tensor;
 template <typename T>
 void launch_build_causal_mask(const unsigned int max_q_len,
                               const unsigned int max_kv_len,
-                              const Tensor<unsigned int> &q_lens,
-                              const Tensor<unsigned int> &kv_lens,
-                              Tensor<T> &mask);
+                              const Tensor<unsigned int> &q_lens,  // (batch_size)
+                              const Tensor<unsigned int> &kv_lens, // (batch_size)
+                              Tensor<T> &mask);                    // (batch_size, q_cache_len, kv_cache_len)
